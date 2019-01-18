4a2e0ad6948d4ea8a02aca9ab2d7df4887766896
maximebergerard <maxime.bergerard@hetic.net>
2019-01-18 16:02:47 +0100

    :art: WebGL start"

--- src/index.js
+++ src/index.js
@@ -1,19 +1,74 @@
 import './css/style.styl'
 
-console.log('Hello booy')
+import * as THREE from 'three'
 
+/**
+ * Sizes
+ */
+const sizes = {}
+sizes.width = window.innerWidth
+sizes.height = window.innerHeight
 
-
-// Hot
-
-if(module.hot)
+window.addEventListener('resize', () =>
 {
-    module.hot.accept()
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
 
-    module.hot.dispose(() =>
-    {
-        console.log('dispose')
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
 
-        document.body.removeChild($image)
-    })
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+})
+
+/**
+ * Cursor
+ */
+const cursor = {}
+cursor.x = 0
+cursor.y = 0
+
+window.addEventListener('mousemove', (_event) =>
+{
+    cursor.x = _event.clientX / sizes.width - 0.5
+    cursor.y = _event.clientY / sizes.height - 0.5
+})
+
+/**
+ * Scene
+ */
+const scene = new THREE.Scene()
+
+/**
+ * Camera
+ */
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 4
+scene.add(camera)
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer()
+renderer.setSize(sizes.width, sizes.height)
+renderer.shadowMap.enabled = true
+document.body.appendChild(renderer.domElement)
+
+/**
+ * Loop
+ */
+const loop = () =>
+{
+    window.requestAnimationFrame(loop)
+
+    // Update camera
+    camera.position.x = cursor.x * 3
+    camera.position.y = - cursor.y * 3
+    camera.lookAt(new THREE.Vector3())
+
+    // Renderer
+    renderer.render(scene, camera)
 }
+loop()
